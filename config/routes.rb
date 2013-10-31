b5da63518f5d86690ac9174a121f15ae42ff81cd
Pugiemonn <pugiemonn@gmail.com>
2013-10-31 23:09:33 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ PugiemonnComV0::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   resources :projects
+  
+  root 'projects#index'
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
