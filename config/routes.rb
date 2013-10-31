b043efa08d46f181f76b6c09602e4f603fa17cb2
Pugiemonn <pugiemonn@gmail.com>
2013-10-31 22:48:28 +0900

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 PugiemonnComV0::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  resources :projects
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
