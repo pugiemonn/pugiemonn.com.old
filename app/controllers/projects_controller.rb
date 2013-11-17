96a3620d89d87c1f0e8c959c4adc01ea0782c7b2
Pugiemonn <pugiemonn@gmail.com>
2013-11-17 14:53:32 +0900

    add new action and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,12 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+  end
 end
