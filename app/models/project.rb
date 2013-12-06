995001c4a3310bedf7da1f6eb504b5b59e37addf
y@su <toyasyu@gmail.com>
2013-12-06 23:55:10 +0900

    localeファイルの移動とmodel,view用のディレクトリ作成他。

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,4 @@
 class Project < ActiveRecord::Base
   has_many :tasks
-  validates :title, 
-    presence: { message: "入力してください" },
-    length: { minimum: 3, message: "短いです" }
+  validates :title, presence: true, length: { minimum: 3 }
 end
