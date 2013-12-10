f7d23be61caf295be961a5ab4362cfe4927d80de
Pugiemonn <pugiemonn@gmail.com>
2013-12-11 02:50:05 +0900

    today's progress

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ PugiemonnComV0::Application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'taks#toggle'
   
   root 'projects#index'
 
