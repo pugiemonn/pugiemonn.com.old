71698b3f773615dcd7ad47fc8b6c2ad7459a1ed1
Pugiemonn <pugiemonn@gmail.com>
2013-12-21 12:35:40 +0900

    add community views

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,4 +2,21 @@ class CommunitiesController < ApplicationController
   def index
     @communities = Community.all
   end
+  
+  def new
+    @community = Community.new
+  end
+  
+  def create
+    @community = Community.new(community_params)
+    if @community.save
+      redirect_to communities_path 
+    else
+      render 'new'
+    end
+  end
+  private
+    def community_params
+      params[:community].permit(:name)
+    end
 end
