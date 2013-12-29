355b4a0029260913e00f9ff1fdc4963daaa28243
Pugiemonn <pugiemonn@gmail.com>
2013-12-29 17:43:22 +0900

    add events actions and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,22 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def event_params
+      params[:event].permit(:title)
+    end
 end
