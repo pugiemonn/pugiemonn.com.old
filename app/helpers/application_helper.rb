bd04c884db8afeedb0124a1ea3ee68216450b583
Pugiemonn <pugiemonn@gmail.com>
2014-01-21 03:07:50 +0900

    add generate title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Pugiemonn.com Sample Page"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
