f5d35bfa1d842f3f3bef28bff173c6d3aeeea512
Pugiemonn <pugiemonn@gmail.com>
2014-03-01 20:37:30 +0900

    add event show action and view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,10 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
     def event_params
       params[:event].permit(:title, :description)
