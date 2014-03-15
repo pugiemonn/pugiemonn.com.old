2bf71b510c87e9e6b920df7c05c665e16779e4f1
Pugiemonn <pugiemonn@gmail.com>
2014-03-16 08:59:58 +0900

    add event attendance action

--- app/controllers/eventattendance_controller.rb
+++ app/controllers/eventattendance_controller.rb
@@ -1,5 +1,14 @@
 class EventattendanceController < ApplicationController
-  def create
+  def new
 
+  end
+
+  def create
+    @eventattendance = Eventattendance.new(event_params)
+    if @eventattendance.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 end
