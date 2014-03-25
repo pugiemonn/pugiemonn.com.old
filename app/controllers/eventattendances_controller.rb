d75c39682ff955bf49b18bd453dec9eeaae8f0fe
Pugiemonn <pugiemonn@gmail.com>
2014-03-25 23:34:24 +0900

    change controller form

---
+++ app/controllers/eventattendances_controller.rb
@@ -0,0 +1,24 @@
+class EventattendancesController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @eventattendance = Eventattendance.new(event_params)
+    if @eventattendance.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def attend
+    @eventattendance = Eventattendance.new(event_params)
+    if @eventattendance.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+end
