a76d741eda41207d2f72818edb7cd6621c555cdd
Pugiemonn <pugiemonn@gmail.com>
2014-03-29 20:55:54 +0900

    add profile edit view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,29 @@
 class ProfilesController < ApplicationController
+
+  before_action :set_profile
+
   def index
-    if user_signed_in?
-      unless current_user.profile
-        Profile.create(user: current_user)
-      end
-    else
-      redirect_to new_user_session_path
-    end
   end
+
+  def update
+    @profile.update(profile_name)
+    redirect_to profiles_path
+  end
+
+  private
+    def set_profile
+      if user_signed_in?
+        unless current_user.profile
+          Profile.create(user: current_user)
+        else
+          @profile = Profile.find_by_user_id(current_user.profile.user_id)
+        end
+      else
+        redirect_to new_user_session_path
+      end
+    end
+
+    def profile_name
+      params[:profile].permit(:name)
+    end
 end
