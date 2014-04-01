3cb026715abc9a0626cf98f6f1274380b7ab3261
Pugiemonn <pugiemonn@gmail.com>
2014-04-02 08:27:11 +0900

    add order option at index action

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,7 +2,7 @@ class MypageController < ApplicationController
 
   def index
     if user_signed_in?
-      @owner_events = Event.find(:all, current_user, limit: 10)
+      @owner_events = Event.find(:all, current_user, limit: 10, order: "created_at DESC")
     end
   end
 end
