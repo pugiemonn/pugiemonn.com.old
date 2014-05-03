cdc04a3fbc6599cb2591f46d470ed8514482d64e
Pugiemonn <pugiemonn@gmail.com>
2014-05-03 20:31:54 +0900

    edit link css

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,7 +2,8 @@ class MypageController < ApplicationController
 
   def index
     if user_signed_in?
-      @owner_events = Event.find(:all, current_user, limit: 10, order: "created_at DESC")
+      @events        = Event.find(:all, current_user, limit: 10, order: 'created_at DESC')
+      #@events_future = Event.find_by_user_id(current_user.id)
     end
   end
 end
