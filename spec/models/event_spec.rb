aebf0037528ebec96bcf94f83c1f542fc087d556
Pugiemonn <pugiemonn@gmail.com>
2014-09-19 08:19:23 +0900

    edit event_spec.rb

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+#describe Event do
+#RSpec.describe Comment, type: :model do
+RSpec.describe Event, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+  context "パラメータが正しいとき" do
+    let(:description) do
+      <<-DESC
+        イベントを作成します
+      DESC
+    end
+    let(:result) do
+
+    end
+  end
 end
