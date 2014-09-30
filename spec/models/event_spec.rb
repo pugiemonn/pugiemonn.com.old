4deae759bc583a40508d0131e6391d8fa3dffae0
Pugiemonn <pugiemonn@gmail.com>
2014-09-30 09:12:58 +0900

    edit event_spec.rb

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,17 +1,11 @@
 require 'spec_helper'
 
-#describe Event do
-#RSpec.describe Comment, type: :model do
-RSpec.describe Event, type: :model do
-  let(:user) { FactoryGirl.create(:user) }
-  context "パラメータが正しいとき" do
-    let(:description) do
-      <<-DESC
-        イベントを作成します
-      DESC
-    end
-    let(:result) do
+describe Event do
 
-    end
+  it "is valid with a description" do
+    event = Event.new(
+      description: "イベント本文"
+    )
+    expect(event).to be_valid
   end
 end
