84749a46e9e4727229ca8bd69ac6d957ded63a64
Pugiemonn <pugiemonn@gmail.com>
2014-10-02 01:31:53 +0900

    edit event_spec.rb

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,8 +4,13 @@ describe Event do
 
   it "is valid with a description" do
     event = described_class.new(
-      description: "イベント本文"
+      description: "イベント本文",
+      title: "イベントタイトル"
     )
     expect(event).to be_valid
+  end
+
+  it "is invalid without a description" do
+    expect(described_class.new(description: nil)).not_to be_valid
   end
 end
