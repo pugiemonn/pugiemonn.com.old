1b5365d039a926231c4af370734c7d3df15657aa
Pugiemonn <pugiemonn@gmail.com>
2014-10-11 02:49:05 +0900

    add factory test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Event do
+  # ファクトリが有効であること
+  it "has a valid factory" do
+    expect(build(:event)).to be_valid
+  end
 
   it "is valid with a description" do
     event = described_class.new(
@@ -13,4 +17,5 @@ describe Event do
   it "is invalid without a description" do
     expect(described_class.new(description: nil)).not_to be_valid
   end
+
 end
